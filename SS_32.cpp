631cec362c91309929ce2b434242fb04bf03ef9f
MalishTulen <87848934+MalishTulen@users.noreply.github.com>
2024-08-20 18:37:43 +0300

    сделал чут-чут красивее но оно все еще не повторяет запрос на ввод после неудачной попытки ввода

--- SS_32.cpp
+++ SS_32.cpp
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 const int ALLZERO = -1;
 const double Epc = 0.000001;
 //Решение квадратных уравнений
-int SS(double a,double b,double c,double *x1,double *x2);
+int SS(double a, double b, double c, double *x1, double *x2);
+
+//-----------------------------------------------------------------------
 int main()
 {
-    double a=0,b=0,c=0;
-    printf("Insert koeff a,b,c:");
-    scanf("%lf %lf %lf",&a,&b,&c);    //Вводятся коэффы a, b и c
-    double x1=0,x2=0;
-    int Amount_of_Square_Roots;             //Вводится Amount_of_Square_Roots
-    Amount_of_Square_Roots=SS(a,b,c,&x1,&x2);         //которая считат колво корней
+    double a = 0, b = 0, c = 0;
+    printf ("Insert koeff a, b, c:");
+    while ((scanf("%lf %lf %lf", &a, &b, &c)) != 3)
+        {
+            a = 0, b = 0, c = 0;
+            printf("Insert koeff a, b, c:");
+            scanf("%lf %lf %lf", &a, &b, &c);
+        }
+    scanf("%lf %lf %lf", &a, &b, &c);
+    double x1 = 0, x2 = 0;
+    int Amount_of_Square_Roots = 0;
+    Amount_of_Square_Roots = SS(a, b, c, &x1, &x2);
     switch(Amount_of_Square_Roots)
     {
         case 0: printf("0 korney");
                 break;
-        case 1: printf("1 koren, x1=%g",x1);
+        case 1: printf("1 koren, x1=%g", x1);
                 break;
-        case 2: printf("2 kornya, %g and %g",x1,x2);
+        case 2: printf("2 kornya, %g and %g", x1, x2);
                 break;
         case ALLZERO: printf("Impressive! Ochen mnogo korney!");
                 break;
@@ -27,11 +36,15 @@ int main()
     }
     return 0;
 }
-int SS(double a,double b,double c,double *x1,double *x2)
+
+//----------------------------------------------------------------
+
+
+int SS(double a, double b, double c, double *x1, double *x2)
 {
-    if (fabs(a)<Epc)
+    if (fabs(a) < Epc)
     {
-        if (fabs(b)<Epc && fabs(c)<Epc)
+        if (fabs(b) < Epc && fabs(c) < Epc)
         {
             return ALLZERO;
         }
@@ -42,22 +55,22 @@ int SS(double a,double b,double c,double *x1,double *x2)
     }
     else
     {
-        double d = b*b-4*a*c;
-        if (d<0)
+        double d = b*b - 4*a*c;
+        if (d < 0)
         {
             return 0;
         }
         else
         {
-            if (fabs(d)<Epc)
+            if (fabs(d) < Epc)
             {
-                *x1=*x2=-b/2/a;
+                *x1 = *x2 = -b/2/a;
                 return 1;
             }
-            if (d>0)
+            if (d > 0)
             {
-                *x1=(-b+sqrt(d))/(2*a);
-                *x2=(-b-sqrt(d))/(2*a);
+                *x1 = (-b+sqrt(d))/(2*a);
+                *x2 = (-b-sqrt(d))/(2*a);
                 return 2;
             }
         }
