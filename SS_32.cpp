2f42d8e0829daa87a5b56261a4fc98f669dd7a1c
MalishTulen <87848934+MalishTulen@users.noreply.github.com>
2024-08-23 13:55:45 +0300

    добавил функцию в мейне заместо ввода символов

--- SS_32.cpp
+++ SS_32.cpp
@@ -28,6 +28,7 @@ int run_testicals ( int test_number, double a, double b, double c,
 int abs ( double x );
 void kvadratka ( double a, double b, double c, double x1, double x2, int amount_of_square_roots );
 void lin_solver ( double b, double c, double x1, int amount_of_square_roots );
+int inputer ( int checker );
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -54,58 +55,42 @@ test test_base[N_kg_TESTA] =
 //-------------------------------------------------------------------------
 int main ()
 {
-    printf ( "~~~~KVADRATKA~~~~\n\nHello!\npress 1 to solve qudratnoe uravnenie\npress 2 to run tests\nYour input:");
     int checker = 0;
-    while ( ( checker = getchar () ) != EOF )
+    switch ( inputer ( checker ) )
     {
-        if ( checker != '1' && checker != '2' )
+        case INPUT_1:
         {
-            while ( getchar() != '\n')
-            {
-                continue;
-            }
-            printf ( "Wrong input! Try again:" );
+            double a = 0, b = 0, c = 0;
+
+            inputin_32 ( 'a', &a );
+            inputin_32 ( 'b', &b );
+            inputin_32 ( 'c', &c );
+
+            //printf ( "%lf %lf %lf", a, b, c );
+
+            double x1 = 0, x2 = 0;
+            int amount_of_square_roots = 0;
+
+            switch_to_answer ( a, b, c, &x1, &x2, amount_of_square_roots );
+            break;
         }
-        else
+        case INPUT_2:
         {
-            switch ( checker )
+            int i = 0;
+            int Rtest_amount = 0;
+            for ( i = 0; i < N_kg_TESTA; i++ )
             {
-                case INPUT_1:
-                {
-                    double a = 0, b = 0, c = 0;
-
-                    inputin_32 ( 'a', &a );
-                    inputin_32 ( 'b', &b );
-                    inputin_32 ( 'c', &c );
-
-                    //printf ( "%lf %lf %lf", a, b, c );
-
-                    double x1 = 0, x2 = 0;
-                    int amount_of_square_roots = 0;
-
-                    switch_to_answer ( a, b, c, &x1, &x2, amount_of_square_roots );
-                    break;
-                }
-                case INPUT_2:
-                {
-                    int i = 0;
-                    int Rtest_amount = 0;
-                    for ( i = 0; i < N_kg_TESTA; i++ )
-                    {
-                        Rtest_amount = Rtest_amount + run_testicals ( test_base[i].test_number, test_base[i].a, test_base[i].b,
-                          test_base[i].c,  test_base[i].x1_expedo,  test_base[i].x2_expedo,
-                         test_base[i].amount_of_square_roots_expedo );
-                    }
-                    printf ( "\n\n*************************************\n*****"
-                    "Amount of right tests: %d/%d******\n*************************************", Rtest_amount, N_kg_TESTA );
-                    break;
-                }
-                default:
-                {
-                    printf("Somehow ERROR in main input\n");
-                    break;
-                }
+                Rtest_amount = Rtest_amount + run_testicals ( test_base[i].test_number, test_base[i].a, test_base[i].b,
+                test_base[i].c,  test_base[i].x1_expedo,  test_base[i].x2_expedo,
+                test_base[i].amount_of_square_roots_expedo );
             }
+            printf ( "\n\n*************************************\n*****"
+            "Amount of right tests: %d/%d******\n*************************************", Rtest_amount, N_kg_TESTA );
+            break;
+        }
+        default:
+        {
+            printf("Somehow ERROR in main input\n");
             break;
         }
     }
@@ -268,4 +253,25 @@ void lin_solver ( double b, double c, double x1, int amount_of_square_roots )
     x1 = -c / b;
     amount_of_square_roots = 1;
     printf ( "%d koren:\nx1 = %lg", amount_of_square_roots, x1 );
+}
+
+int inputer ( int checker )
+{
+    printf ( "~~~~KVADRATKA~~~~\n\nHello!\npress 1 to solve qudratnoe uravnenie\npress 2 to run tests\nYour input:");
+    while ( ( checker = getchar () ) != EOF )
+    {
+        if ( checker != '1' && checker != '2' )
+        {
+            while ( getchar() != '\n')
+            {
+                continue;
+            }
+            printf ( "Wrong input! Try again:" );
+        }
+        else
+        {
+            return checker;
+        }
+    }
+    return 0;
 }
